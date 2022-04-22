88034ab77c4e98fc1131271554df15d3b5a95bc3
Jaroslav <garasym@gmail.com>
2022-04-22 10:07:14 +0300

    maven project

---
+++ src/test/java/com/softserve/edu/Simple.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+import org.testng.annotations.Test;
+
+public class Simple {
+
+    @Test
+    public void testSmpl() {
+        System.out.println("***Simple");
+    }
+}
