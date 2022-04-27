fca615ce105eb722ce033afd1e7ca76e24683210
asIgnat <alexandr.ihnatchenko@gmail.com>
2022-04-27 13:46:28 +0300

    hw1

--- src/main/java/com/softserve/hw1/Brackets.java
+++ src/main/java/com/softserve/hw1/Brackets.java
@@ -1,16 +1,15 @@
 package com.softserve.hw1;
-
 public class Brackets {
     public boolean verifyBrackets(String text) {
         int closed = 0;
         int opened = 0;
         String[] atext = text.split("");
         for (int i = 0; i < atext.length; i++) {
-            if (atext[0] == ")") {
+            if (atext[0].equals(")")) {
                 return false;
-            } else if (atext[i] == "(") {
+            } else if (atext[i].equals("(")) {
                 opened = opened + 1;
-            } else if (atext[i] == ")") {
+            } else if (atext[i].equals(")")) {
                 closed = closed + 1;
             }
         }
