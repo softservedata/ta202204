7cde9aff728b6b2ee090fa5d904105cffde4666b
irynabevz <bewz1006@gmail.com>
2022-05-08 23:59:16 +0300

    Homework 3.

--- src/test/java/com/softserve/edu/homework03/NumberOfBracketsTest.java
+++ src/test/java/com/softserve/edu/homework03/NumberOfBracketsTest.java
@@ -2,15 +2,18 @@ package com.softserve.edu.homework03;
 
 import com.softserve.edu.homework01.VerifyBrackets;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class NumberOfBracketsTest {
 
+
     @Test
-    public void testCountBracketsTrue_1() {
+    public void testCountBrackets() {
         VerifyBrackets VerifyBracketsMock = Mockito.spy(new VerifyBrackets());
         Mockito.when(VerifyBracketsMock.isCorrectBrackets(anyString())).thenReturn(true);
         Mockito.doCallRealMethod().when(VerifyBracketsMock).countBrackets(anyString());
@@ -18,7 +21,6 @@ public class NumberOfBracketsTest {
         Assert.assertEquals(6, VerifyBracketsMock.countBrackets("(((str)))"));
         Assert.assertEquals(1, VerifyBracketsMock.countBrackets("( String"));
         Assert.assertEquals(8, VerifyBracketsMock.countBrackets("(((str)))()"));
-        Assert.assertEquals(0, VerifyBracketsMock.countBrackets(" "));
     }
 
 }
