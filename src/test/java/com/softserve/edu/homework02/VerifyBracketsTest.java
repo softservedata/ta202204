513bfcb3bb0b25903dec7affa27fea9241be0bf8
irynabevz <bewz1006@gmail.com>
2022-05-08 19:48:01 +0300

    Homework 2.

--- src/test/java/com/softserve/edu/homework02/VerifyBracketsTest.java
+++ src/test/java/com/softserve/edu/homework02/VerifyBracketsTest.java
@@ -5,21 +5,46 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class VerifyBracketsTest {
+
     VerifyBrackets testBrackets = new VerifyBrackets();
 
     @Test
-    public void testIsCorrectBracketsTrue() {
+    public void testIsCorrectBracketsTrue_1() {
         Assert.assertTrue(testBrackets.isCorrectBrackets("Correct (brackets)()"));
+    }
+
+    @Test
+    public void testIsCorrectBracketsTrue_2() {
         Assert.assertTrue(testBrackets.isCorrectBrackets("()"));
+    }
+
+    @Test
+    public void testIsCorrectBracketsTrue_3() {
         Assert.assertTrue((testBrackets.isCorrectBrackets("((()))")));
+    }
+
+    @Test
+    public void testIsCorrectBracketsTrue_4() {
         Assert.assertTrue(testBrackets.isCorrectBrackets("Text {(with) [several] type of (brackets)}"));
     }
 
     @Test
-    public void testIsCorrectBracketsFalse(){
+    public void testIsCorrectBracketsFalse_1() {
         Assert.assertFalse(testBrackets.isCorrectBrackets("((("));
+    }
+
+    @Test
+    public void testIsCorrectBracketsFalse_2() {
         Assert.assertFalse(testBrackets.isCorrectBrackets("Test (text "));
+    }
+
+    @Test
+    public void testIsCorrectBracketsFalse_3() {
         Assert.assertFalse(testBrackets.isCorrectBrackets(")(("));
+    }
+
+    @Test
+    public void testIsCorrectBracketsFalse_4() {
         Assert.assertFalse(testBrackets.isCorrectBrackets("((())()"));
     }
 }
