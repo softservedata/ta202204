edb404b893b14bb469aef4cec9420cc1bea0c12c
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-06-14 06:59:18 +0300

    OlehP_homework12

---
+++ src/test/java/com/softserve/edu/opencart/pages/CurrencyDropdown.java
@@ -0,0 +1,8 @@
+package com.softserve.edu.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CurrencyDropdown {
+    private WebDriver driver;
+
+}
---
+++ src/test/java/com/softserve/edu/opencart/tests/CurrencyTest.java
@@ -0,0 +1,34 @@
+package com.softserve.edu.opencart.tests;
+
+import com.softserve.edu.opencart.data.Currencies;
+import com.softserve.edu.opencart.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CurrencyTest extends TestRunner {
+
+    @DataProvider(parallel = true)
+    public Object[][] searchCurrencies() {
+        return new Object[][] { 
+            { Currencies.EURO },
+            //{ Currencies.POUND_STERLING },
+        };
+    }
+    
+    @Test(dataProvider = "searchCurrencies")
+    public void checkFirst(Currencies currency) {
+        //
+        // Steps
+        HomePage homePage = loadApplication()
+                //.chooseCurrency("US Dollar");
+                .chooseCurrency(currency);
+                //.scrollToProduct(searchProduct.getProduct());
+        presentationSleep();
+        //
+        // Check
+        Assert.assertTrue(homePage.getCurrencyText().contains("€"));
+        //
+        presentationSleep(4);
+    }
+}
