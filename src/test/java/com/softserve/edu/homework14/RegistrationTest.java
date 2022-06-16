541941df752c79daec50dd25a3489c7fbfe97d8f
tyutyunnykolena <olenatyut@gmail.com>
2022-06-16 22:42:38 +0300

    hw14 check registration

--- src/test/java/com/softserve/edu/homework14/RegistrationTest.java
+++ src/test/java/com/softserve/edu/homework14/RegistrationTest.java
@@ -9,30 +9,19 @@ package com.softserve.edu.homework14;
     Запушати код на github у свою вітку.
  */
 
-import com.softserve.edu.opencart.data.IUser;
 import com.softserve.edu.opencart.data.UserRepository;
 import com.softserve.edu.opencart.pages.HomePage;
 import com.softserve.edu.opencart.pages.RegisterPage;
 import com.softserve.edu.opencart.tests.TestRunnerFirst;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class RegistrationTest extends TestRunnerFirst {
-
-    @DataProvider
-    public Object[][] dataSuccessful() {
-        return new Object[][]{
-            {UserRepository.getNewUser()},
-        };
-    }
-
-    @Test(dataProvider = "dataSuccessful")
-    //public void checkSuccessful(String email, String password, String firstName) {
-    public void checkSuccessful(IUser validNewUser) {
+    @Test
+    public void checkSuccessful() {
         HomePage homePage = loadApplication();
         RegisterPage registerPage = homePage.gotoRegisterPage();
-        registerPage.successfulRegister(validNewUser);
+        registerPage.successfulRegister(UserRepository.getNewUser());
 
         Assert.assertEquals(registerPage.getSuccessMessageText(), registerPage.successRegistrationMessage);
     }
